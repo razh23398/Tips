fc837a3e646670a3a23fea998dc27c706481104f
Raz Hershko <115927546+razh23398@users.noreply.github.com>
2023-04-22 17:45:14 +0300

    TIPS FOR LEADER (WHEN PASS THE 10 TIP) BUG FIX

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ function cliked(){
     let bigTipView = (creditTip + cashTip) - hafrasha // חישוב של סך הטיפים פחות ההפרשה = סך הטיפ לחלוקה
     document.querySelector("#bigTipView").innerHTML = bigTipView 
     let smallTipView = bigTipView / totalHours; // כמות הטיפ שנשאר לחלוקה לחלק למספר השעות שעבדו העובדים
-    document.querySelector("#smallTipView").innerHTML = smallTipView
-
+    console.log(smallTipView)
+    if (smallTipView >= 10){
+        totalHours = totalHours + 4
+        smallTipView = bigTipView / totalHours
+        document.querySelector("#smallTipView").innerHTML = smallTipView
     document.querySelector("#work11").innerHTML = smallTipView * parseFloat(document.querySelector("#work1").value)
     document.querySelector("#work22").innerHTML = smallTipView * parseFloat(document.querySelector("#work2").value)
     document.querySelector("#work33").innerHTML = smallTipView * parseFloat(document.querySelector("#work3").value)
@@ -28,5 +31,17 @@ function cliked(){
     document.querySelector("#work66").innerHTML = smallTipView * parseFloat(document.querySelector("#work6").value)
     document.querySelector("#work77").innerHTML = smallTipView * parseFloat(document.querySelector("#work7").value)
     document.querySelector("#work88").innerHTML = smallTipView * parseFloat(document.querySelector("#work8").value)
-
+    document.querySelector("#work99").innerHTML = smallTipView * 4
+    } else {
+        document.querySelector("#smallTipView").innerHTML = smallTipView
+    document.querySelector("#work11").innerHTML = smallTipView * parseFloat(document.querySelector("#work1").value)
+    document.querySelector("#work22").innerHTML = smallTipView * parseFloat(document.querySelector("#work2").value)
+    document.querySelector("#work33").innerHTML = smallTipView * parseFloat(document.querySelector("#work3").value)
+    document.querySelector("#work44").innerHTML = smallTipView * parseFloat(document.querySelector("#work4").value)
+    document.querySelector("#work55").innerHTML = smallTipView * parseFloat(document.querySelector("#work5").value)
+    document.querySelector("#work66").innerHTML = smallTipView * parseFloat(document.querySelector("#work6").value)
+    document.querySelector("#work77").innerHTML = smallTipView * parseFloat(document.querySelector("#work7").value)
+    document.querySelector("#work88").innerHTML = smallTipView * parseFloat(document.querySelector("#work8").value)
+    document.querySelector("#work99").innerHTML = 0
+    }
 }
